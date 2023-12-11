ec5f15b71da03969860c194bd3377afe0d676924
cis690 <cis690@gmail.com>
2023-12-11 16:52:50 -0600

    ..

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Patients = require('../models/Patients');
+const Patients = require('../models/patients');
 const authMiddleware = require('../middleware/auth');
 
 
